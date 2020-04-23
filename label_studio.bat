3a8576c67e3e17b29cf3a76fdf6f1c64e2c1727e
evanmiller29 <evanmiller29@gmail.com>
2020-04-23 20:43:41 +0100

    Adding order number to bat and config file for label studio

--- label_studio.bat
+++
@@ -1,6 +0,0 @@
-call conda create --name rasp_pi_train python=3.7 -y
-call activate rasp_pi_train
-call pip install -r requirements_windows.txt
-
-call label-studio init train_detection
-call label-studio start train_detection
